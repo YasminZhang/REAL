828bff22c1259b98831579062c05968505a37d84
tianyu <tianyuchen99@gmail.com>
2025-08-30 21:09:17 +0000

    add some codex config

--- azure/codex_install.sh
+++ azure/codex_install.sh
@@ -5,3 +5,9 @@ sudo apt-get install -y nodejs
 
 sudo npm install -g @openai/codex
 codex --sandbox workspace-write --config sandbox_workspace_write.network_access=true
+
+
+# config.toml
+# [mcp_servers.context7]
+# args = ["-y", "@upstash/context7-mcp", "--api-key", "ctx7sk-0478d348-d670-439f-a4a4-3bb073445100"]
+# command = "npx"
