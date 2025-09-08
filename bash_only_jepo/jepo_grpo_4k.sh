974ab4fd0a638289106be33f32df8b9ce1d85f87
tianyu <tianyuchen99@gmail.com>
2025-09-08 05:03:48 +0000

    add kl fosource /opt/conda/bin/activate jepo

--- bash_only_jepo/jepo_grpo_4k.sh
+++ bash_only_jepo/jepo_grpo_4k.sh
@@ -3,7 +3,7 @@ set -xeuo pipefail
 
 project_name='JEPO_token'
 #exp_name='deepscaler-1.5b-2k-format-test-g1-delimiter-token-math'
-exp_name="dapo_grpo_4k"
+exp_name="dapo_grpo_4k_kl"
 
 adv_estimator=grpo
 
@@ -40,7 +40,7 @@ use_grpo=True
 jepo_delimiter="\\boxed\{"
 jepo_format_penalty=10
 jepo_beta_supp=0.001
-jepo_beta_kl=0.0
+jepo_beta_kl=0.001
 jepo_entropy_coeff=0.0
 jepo_buffer_size=64 # number of questions
 jepo_steps=1
