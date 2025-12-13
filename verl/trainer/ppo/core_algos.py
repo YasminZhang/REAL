56483e19b267a8544eff2d4ff244fd18e96b08e4
tianyu <tianyuchen99@gmail.com>
2025-12-13 21:41:49 +0000

    talk to tianyu

--- verl/trainer/ppo/core_algos.py
+++ verl/trainer/ppo/core_algos.py
@@ -974,7 +974,7 @@ def compute_policy_loss_gpg(old_log_prob, log_prob, advantages, response_mask, l
     pg_losses = -log_prob * advantages
     pg_loss = agg_loss(loss_mat=pg_losses, loss_mask=response_mask, loss_agg_mode=loss_agg_mode)
     if extra_loss is not None:
-        pg_loss = pg_loss + extra_loss.sum()
+        pg_loss = pg_loss + extra_loss.mean()
     return pg_loss, torch.tensor(0.0), torch.tensor(0.0), torch.tensor(0.0)
 
 
