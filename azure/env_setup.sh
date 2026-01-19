2cfe1f40b41819aa3c6be77169b109921670d6bc
Your Name <you@example.com>
2026-01-19 09:45:19 +0000

    update env

--- azure/env_setup.sh
+++ azure/env_setup.sh
@@ -1,9 +1,9 @@
-# conda create -n jepo python=3.12 -y
-# source activate jepo
+conda create -n jepo python=3.12 -y
+source activate jepo
 pip install -e .
 pip3 install -e .[vllm]
 # pip install flash-attn
-pip install flash-attn --no-build-isolation
+pip install flash-attn==2.8.1 --no-build-isolation
 pip install "transformers<4.54.0"
 pip install "ray[default]"  
 pip install ray==2.38
