d6c772b1cbe360635487ef38a6a5a4bcdb5c281a
tianyu <tianyuchen99@gmail.com>
2026-01-23 17:20:22 +0000

    delete target module

--- tianyu_bash/real_32b_ray_submit_from_raft_full_lr1e-6.sh
+++ tianyu_bash/real_32b_ray_submit_from_raft_full_lr1e-6.sh
@@ -215,7 +215,6 @@ ray job submit \
     actor_rollout_ref.model.trust_remote_code=True \
     actor_rollout_ref.rollout.load_format="safetensors" \
     +actor_rollout_ref.rollout.trust_remote_code=True \
-    actor_rollout_ref.model.target_modules=all-linear \
     actor_rollout_ref.model.use_shm=False \
     actor_rollout_ref.actor.optim.lr=${lr} \
     actor_rollout_ref.actor.optim.lr_warmup_steps=0 \
